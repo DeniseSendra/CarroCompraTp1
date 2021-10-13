b6fb6fcc24057419f8dda9cebf0934d7be134b94
DeniseSendra <denisesmusic@hotmail.com>
2021-10-13 18:18:25 -0300

    Solucionado el js (aunque sube la pág al tocar el botón), falta agregar el diseño responsive

--- carroCompra.js
+++ carroCompra.js
@@ -1,3 +1,29 @@
-var CantidadEntradas = "#CantidadEntradas"
+var form = document.getElementById("form2"); 
+
+var numEntradas = document.getElementById("calcEntradas");
+
+var resultadoAmostrar = document.getElementById("totalYdes");
+
+var mostrame = document.getElementById("mostrame");
+
+mostrame.addEventListener('click', resultadoCompra1());
 
 
+function resultadoCompra1(){
+ 
+  var numEntradas = document.getElementById("calcEntradas").value; 
+  totalEntradas = numEntradas * 200;
+  resultadoCompra = "0"; 
+  
+  var descuentoValue = document.getElementById("inputState").value;
+
+  if(descuentoValue == 1){
+      resultadoCompra = totalEntradas - (totalEntradas * 0.8);
+  } else if (descuentoValue == 2){
+    resultadoCompra = totalEntradas - (totalEntradas * 0.5);
+  }else{
+      resultadoCompra = totalEntradas - (totalEntradas * 0.15);
+  }
+ 
+ resultadoAmostrar.innerText= "Total a pagar " + resultadoCompra + " $";
+}
